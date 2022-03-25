09e08dfb57be4b49456ddf8f3fe311064f77c694
OldSaltFish <3295696322@qq.com>
2022-03-25 08:52:46 +0800

    Site updated: 2022-03-25 08:52:46

---
+++ js/test.js
@@ -0,0 +1,8 @@
+<div class="social-share"></div>
+
+<!-- share.css -->
+<link rel="stylesheet" href="dist/css/share.min.css">
+
+<!-- share.js -->
+<script src="dist/js/share.min.js"></script>
+// 当你使用类名为 `social-share` 或者 `social-share` 时不需要手动初始化
